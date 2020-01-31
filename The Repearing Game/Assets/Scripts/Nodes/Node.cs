9e84c66427faa078829cc33d76e6ef85f2df4f67
Penttilä Tuisku TTK19SP <tuiskupenttila@kamk.fi>
2020-01-31 23:23:56 +0200

    Made rudementary first version of hte node system.

---
+++ The Repearing Game/Assets/Scripts/Nodes/Node.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Node : MonoBehaviour 
+{
+    public Transform cameraPosition;
+    public List<Node> reachableNodes = new List<Node>();
+    //<lists> in angle brackets,[arrays] in square brackets
+
+
+    [HideInInspector]
+    public Collider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    void OnMouseDown()
+    {
+        Camera.main.transform.position = cameraPosition.position;
+        Camera.main.transform.rotation = cameraPosition.rotation;
+    }
+
+}
